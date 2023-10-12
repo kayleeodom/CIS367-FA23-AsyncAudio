61daa49d9d2c83a5143bf84673ca65a8f3f2623f
kayleeodom <kaylee.odom@students.fhu.edu>
2023-10-11 22:11:06 -0500

    almost done

--- main.js
+++ main.js
@@ -15,6 +15,8 @@ const artist = document.getElementById('artist');
 const currTime = document.querySelector('#currTime');
 const durTime = document.querySelector('#durTime');
 
+const artistName = document.getElementById('artistName');
+
 // load songs
 const songs = ['Aidan','Autumn Sun','Best Part of Me','Better Days','I Cant Make You Love Me Cover','Just Relax','Paranormal Is Real','Perfect','Polarity','Your Shoulder'];
 
@@ -24,9 +26,12 @@ const artists = ['Jonathan Ceaser','Bryce Greene','The Dunwells','Lakey','Bryce 
 
 // keep track of song
 let songIndex = 0;
+let artistIndex = 0;
 
 // load song details into DOM
 loadSong(songs[songIndex]);
+
+loadArtist(artists[artistIndex]);
 
 // song details
 function loadSong(song){
@@ -34,6 +39,10 @@ function loadSong(song){
   audio.src = `songs/${song}.mp3`;
   cover.src = `albumart/${song}.jpg`;
   // artist
+}
+
+function loadArtist(artist){
+  artistName.innerText = artist;
 }
 
 // Play Song
@@ -57,11 +66,14 @@ function pauseSong(){
 // Previous Song
 function prevSong(){
     songIndex--;
+    artistIndex--;
 
     if(songIndex < 0) {
         songIndex = songs.length - 1;
+        artistIndex = artists.length -1;
     }
 
+    loadArtist(artists[artistIndex]);
     loadSong(songs[songIndex]);
 
     playSong();
@@ -70,11 +82,14 @@ function prevSong(){
 // Next Song
 function nextSong(){
     songIndex++;
+    artistIndex++;
 
   if (songIndex > songs.length - 1) {
     songIndex = 0;
+    artistIndex = 0;
   }
 
+  loadArtist(artists[artistIndex])
   loadSong(songs[songIndex]);
 
   playSong();
@@ -97,7 +112,70 @@ function setProgress(e) {
     audio.currentTime = (clickX / width) * duration;
 }
 
-// Duration & currentTime for Time of Song
+//get duration & currentTime for Time of song
+function DurTime (e) {
+	const {duration,currentTime} = e.srcElement;
+	var sec;
+	var sec_d;
+
+	// define minutes currentTime
+	let min = (currentTime==null)? 0:
+	 Math.floor(currentTime/60);
+	 min = min <10 ? '0'+min:min;
+
+	// define seconds currentTime
+	function get_sec (x) {
+		if(Math.floor(x) >= 60){
+			
+			for (var i = 1; i<=60; i++){
+				if(Math.floor(x)>=(60*i) && Math.floor(x)<(60*(i+1))) {
+					sec = Math.floor(x) - (60*i);
+					sec = sec <10 ? '0'+sec:sec;
+				}
+			}
+		}
+    else{
+		 	sec = Math.floor(x);
+		 	sec = sec <10 ? '0'+sec:sec;
+		}
+	} 
+
+	get_sec (currentTime,sec);
+
+	// change currentTime DOM
+	currTime.innerHTML = min +':'+ sec;
+
+	// define minutes duration
+	let min_d = (isNaN(duration) === true)? '0':
+		Math.floor(duration/60);
+	 min_d = min_d <10 ? '0'+min_d:min_d;
+
+
+	 function get_sec_d (x) {
+		if(Math.floor(x) >= 60){
+			
+			for (var i = 1; i<=60; i++){
+				if(Math.floor(x)>=(60*i) && Math.floor(x)<(60*(i+1))) {
+					sec_d = Math.floor(x) - (60*i);
+					sec_d = sec_d <10 ? '0'+sec_d:sec_d;
+				}
+			}
+		}
+    else{
+		 	sec_d = (isNaN(duration) === true)? '0':
+		 	Math.floor(x);
+		 	sec_d = sec_d <10 ? '0'+sec_d:sec_d;
+		}
+	} 
+
+	// define seconds duration
+	
+	get_sec_d (duration);
+
+	// change duration DOM
+	durTime.innerHTML = min_d +':'+ sec_d;
+		
+};
 
 // Event Listeners
 playBtn.addEventListener('click', () => {
